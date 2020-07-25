3231ff4acce7ad88d1bda14675dc4e652b393a07
Semin Park <SPark9625@gmail.com>
2020-07-25 14:29:55 +0900

    Implement CRUD operations

--- src/api/logs.js
+++ src/api/logs.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/add', async (req, res, next) => {
     try {
         const logEntry = new LogEntry(req.body);
         const createdEntry = await logEntry.save();
@@ -23,6 +23,44 @@ router.post('/', async (req, res, next) => {
         console.log(err.name);
         if (err.name === 'ValidationError')
             res.status(422);
+        next(err);
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const entry = await LogEntry.findById(req.params.id)
+        if (entry === null) {
+            throw new Error(`(ID) ${req.params.id} does not exist.`)
+        }
+        res.json(entry);
+    } catch (err) {
+        console.log(err.name);
+        res.status(404);
+        next(err);
+    }
+});
+
+router.post('/update/:id', async (req, res, next) => {
+    try {
+        const updatedEntry = await LogEntry.findByIdAndUpdate(req.params.id, req.body, {
+            new: true, useFindAndModify: false
+        });
+        res.json(updatedEntry);
+    } catch (err) {
+        console.log(err.name);
+        res.status(400);
+        next(err);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await LogEntry.findByIdAndDelete(req.params.id)
+        res.json('Deleted Successfully');
+    } catch (err) {
+        console.log(err.name);
+        res.status(400);
         next(err);
     }
 });
