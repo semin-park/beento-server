a203b010b64a42679976a03812f1b2335282f0b7
Semin Park <SPark9625@gmail.com>
2020-07-29 16:47:15 +0900

    Allow CORS for all

--- src/index.js
+++ src/index.js
@@ -17,9 +17,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 app.use(morgan('common'));
 app.use(helmet());
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/logs', logs);
